f4de286f89d74b48cd2f6265ae56f771074ba53e
Rafael Anton <anton.rafael10@gmail.com>
2025-07-25 09:31:18 -0300

    Orientação a objetos

---
+++ csharpclasses/Aula 2/PointXYZ.cs
@@ -0,0 +1,62 @@
+﻿namespace csharpclasses.Aula_2
+{
+	internal class PointXYZ
+	{
+		public double X { get; private set; } 
+		public double Y { get; private set; }
+		public double Z { get; private set; }
+
+		public PointXYZ(double x, double y, double z)
+		{
+			X = x;
+			Y = y;
+			Z = z;
+		}
+
+		public double GetX()
+		{
+			return X;
+		}
+		public void SetX(double x)
+		{
+			if (x > 100)
+			{
+				x = 100;
+			}
+
+			X = x;			
+		}
+		public void SetY(double y)
+		{
+			if (y > 100)
+			{
+				y = 100;
+			}
+
+			Y = y;
+		}
+		public void SetZ(double z)
+		{
+			if (z > 100)
+			{
+				z = 100;
+			}
+
+			Z = z;
+		}
+
+		//d=√((x_2-x_1)²+(y_2-y_1)²+(z_2 - z_1)²
+		public double GetDistance(PointXYZ otherPoint)
+		{
+			double distx = Math.Pow(X - otherPoint.X, 2);
+			double disty = Math.Pow(Y - otherPoint.Y, 2);
+			double distz = Math.Pow(Z - otherPoint.Z, 2);
+
+			double sum = distx + disty + distz;
+
+			double distance = Math.Sqrt(sum);
+
+			return distance;
+		}
+	}
+}
---
+++ csharpclasses/Aula 2/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace csharpclasses.Aula_2
+{
+	internal class Program
+	{
+		public static void Main()
+		{
+			PointXYZ point1 = new PointXYZ(2, 2, 2);
+
+			PointXYZ point2 = new PointXYZ(4, 4, 4);
+
+			double distance = point1.GetDistance(point2);
+
+			Console.WriteLine( $"A distância entre os pontos é de: {distance} ." );
+
+		}
+	}
+}
